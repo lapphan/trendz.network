b5a3f55edd617aded998086965e345630ab7aaac
SuperMido <supermido1996@gmail.com>
2020-07-06 15:33:00 +0700

    Change nodemailer-v3 to nodemailer

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,13 +1,13 @@
 module.exports = ({env}) => ({
     email: {
-        provider: "nodemailer-v3",
-        providerOptions: {},
-        settings: {
+        provider: "nodemailer",
+        providerOptions: {
             host: 'mail.smtp2go.com',
             port: 2525,
             username: 'vietnamgamingsetup',
             password: 'Rubbed-Sudden-Sepia0-Reheat-Striving',
-            secure: false
-        }
+            secure: false,
+            authMethod: "SMTP"
+        },
     }
 })
