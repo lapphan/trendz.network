8f78a29c221b000295d0221ed2d3fb9814829df2
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2020-09-01 08:38:21 +0700

    remove GetInitialProps from withApollo & add transition effect

--- client/components/Layout/index.js
+++ client/components/Layout/index.js
@@ -3,10 +3,12 @@ import Header from "../Header";
 import Footer from "../Footer";
 import { useAuth } from "../../context/userContext";
 import { useRouter } from "next/router";
-
+import Transition from "../PageTransition";
 
 const Layout = (props) => {
   const { state } = useAuth();
+  const router = useRouter();
+  const { pathname } = router;
   const [isLoggedIn, setLoggedIn] = useState(false);
   useEffect(() => {
     if (state.jwt !== "") {
@@ -17,18 +19,22 @@ const Layout = (props) => {
     return (
       <div>
         <Header />
-        <main>
-          <div className="section">{props.children}</div>
-        </main>
+        <Transition location={pathname}>
+          <main>
+            <div className="section">{props.children}</div>
+          </main>
+        </Transition>
       </div>
     );
   } else
     return (
       <div>
         <Header />
+        <Transition location={pathname}>
           <main>
             <div>{props.children}</div>
           </main>
+        </Transition>
         <Footer />
       </div>
     );
--- client/utils/ApolloSetup/withApollo.js
+++ client/utils/ApolloSetup/withApollo.js
@@ -16,49 +16,6 @@ export default (App) => {
     static propTypes = {
       apolloState: PropTypes.object.isRequired,
     };
-    static async getInitialProps(ctx) {
-      const {
-        AppTree,
-        ctx: { req, res },
-      } = ctx;
-      const headers = req ? req.headers : {};
-      const apollo = initApollo({}, headers);
-      ctx.ctx.apolloClient = apollo;
-      let appProps = {};
-      if (App.getInitialProps) {
-        appProps = await App.getInitialProps(ctx);
-      }
-      if (res && res.finished) {
-        // When redirecting, the response is finished.
-        // No point in continuing to render
-        return {};
-      }
-      if (typeof window === "undefined") {
-        // Run all graphql queries in the component tree
-        // and extract the resulting data
-        try {
-          // Run all GraphQL queries
-          await getDataFromTree(
-            <AppTree {...appProps} apolloClient={apollo} />
-          );
-        } catch (error) {
-          // Prevent Apollo Client GraphQL errors from crashing SSR.
-          // Handle them in components via the data.error prop:
-          // https://www.apollographql.com/docs/react/api/react-apollo.html#graphql-query-data-error
-          console.error("Error while running `getDataFromTree`", error);
-        }
-        // getDataFromTree does not call componentWillUnmount
-        // head side effect therefore need to be cleared manually
-        Head.rewind();
-      }
-      // Extract query data from the Apollo's store
-      const apolloState = apollo.cache.extract();
-      return {
-        ...appProps,
-        headers,
-        apolloState,
-      };
-    }
     constructor(props) {
       super(props);
       // `getDataFromTree` renders the component first, the client is passed off as a property.
