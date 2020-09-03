fcd395aebb0fc866b114938543a95276f18e000c
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2020-09-03 17:18:57 +0700

    Fix Dashboard reload

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react";
 import { useAuth } from "../context/userContext";
-import Router, { withRouter } from "next/router";
-import dynamic from "next/dynamic";
-const Customer = dynamic(() => import("../components/Dashboards/Customer"));
-const Influencer = dynamic(()=> import("../components/Dashboards/Influencer"))
-const Employee = dynamic(()=>import("../components/Dashboards/Employee"))
+import Router from "next/router";
+//import dynamic from "next/dynamic";
+// const Customer = dynamic(() => import("../components/Dashboards/Customer"));
+// const Influencer = dynamic(()=> import("../components/Dashboards/Influencer"))
+// const Employee = dynamic(()=>import("../components/Dashboards/Employee"))
+
+import Customer from "../components/Dashboards/Customer"
+import Influencer from "../components/Dashboards/Influencer"
+import Employee from "../components/Dashboards/Employee"
+
 const Dashboard = () => {
   const { state } = useAuth();
 
@@ -21,4 +26,4 @@ const Dashboard = () => {
   );
 };
 
-export default withRouter(Dashboard);
+export default Dashboard;
